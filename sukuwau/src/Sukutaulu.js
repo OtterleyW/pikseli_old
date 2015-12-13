4c9fd07fc053de6f13ce6621e515a3c4296ea771
Vesa Laakso <vesa.laakso@venuu.fi>
2015-12-13 17:52:45 +0200

    Toimiva sukutaulu JSONista

--- sukuwau/src/Sukutaulu.js
+++ sukuwau/src/Sukutaulu.js
@@ -1,7 +1,118 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes as T } from 'react';
+
+const layoutStyles = {
+  container: {},
+  sukulainen: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: 1
+  },
+  heppainfo: {
+    display: 'flex',
+    padding: '5px',
+    flexGrow: 1,
+    width: '0'
+  },
+  heppa: {
+    width: '100%'
+  },
+  sukuinfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1
+  }
+};
+
+const borderColor = '#333';
+const lookStyles = {
+  container: {
+    borderTop: `1px solid ${borderColor}`,
+    borderLeft: `1px solid ${borderColor}`
+  },
+  heppainfo: {
+    borderBottom: `1px solid ${borderColor}`,
+    borderRight: `1px solid ${borderColor}`
+  },
+  sukulainen: {
+    backgroundColor: 'rgba(0, 100, 0, 0.15)'
+  },
+  heppa: {
+    textAlign: 'center'
+  }
+};
+
+const styles = Object.keys(layoutStyles).reduce((endStyles, key) => ({
+  ...endStyles,
+  [key]: {
+    ...layoutStyles[key],
+    ...lookStyles[key]
+  }
+}), {});
+
+class Sukulainen extends Component {
+  static propTypes = {
+    nimi: T.string,
+    children: T.node,
+    polvi: T.number.isRequired,
+    pituus: T.number.isRequired
+  }
+
+  render() {
+    const {nimi, children, polvi, pituus} = this.props;
+
+    const sukuinfoTyylit = {
+      ...styles.sukuinfo,
+      flexGrow: pituus - polvi
+    };
+
+    return (
+      <div style={styles.sukulainen}>
+        <div style={styles.heppainfo}>
+          <div style={styles.heppa}>
+            {nimi}
+          </div>
+        </div>
+        {children && <div style={sukuinfoTyylit}>{children}</div>}
+      </div>
+    );
+  }
+}
+
+const suvunPituus = (sukuObj, pituus = 0) => {
+  const { mother, father } = sukuObj;
+  if (mother && father) {
+    return Math.max(suvunPituus(mother, pituus + 1), suvunPituus(father, pituus + 1));
+  }
+  return pituus;
+};
 
 export default class Sukutaulu extends Component {
   render() {
-    return <div>Hello, world!</div>;
+    const { suku } = this.props;
+    const pituus = suvunPituus(suku);
+
+    return (
+      <div style={{ overflow: 'hidden' }}>
+        <div style={styles.container}>
+          {this._renderHeppa(suku, pituus)}
+        </div>
+      </div>
+    );
+  }
+
+  _renderHeppa(heppa, pituus, polvi = 0) {
+    const { name: nimi, father, mother } = heppa;
+
+    if (father && mother) {
+      return (
+        <Sukulainen nimi={nimi} polvi={polvi} pituus={pituus} key={nimi}>
+          {this._renderHeppa(father, pituus, polvi + 1)}
+          {this._renderHeppa(mother, pituus, polvi + 1)}
+        </Sukulainen>
+      );
+    }
+    else {
+      return <Sukulainen nimi={nimi} polvi={polvi} pituus={pituus} key={nimi} />;
+    }
   }
 }
--- sukuwau/src/index.js
+++ sukuwau/src/index.js
@@ -2,4 +2,33 @@ import React from 'react';
 import { render } from 'react-dom';
 import Sukutaulu from './Sukutaulu';
 
-render(<Sukutaulu />, document.getElementById('root'));
+const sukuJson = {
+  name: 'Heppa',
+  father: {
+    name: 'Isä',
+    father: {
+      name: 'Isän isä',
+      father: { name: 'Isän isän isä' },
+      mother: { name: 'Isän isän emä' } },
+    mother: {
+      name: 'Isän emä',
+      father: { name: 'Isän emän isä' },
+      mother: { name: 'Isän emän emä' }
+    }
+  },
+  mother: {
+    name: 'Emä',
+    father: {
+      name: 'Emän isä',
+      father: { name: 'Emän isän isä' },
+      mother: { name: 'Emän isän emä' }
+    },
+    mother: {
+      name: 'Emän emä',
+      father: { name: 'Emän emän isä' },
+      mother: { name: 'Emän emän emä' }
+    }
+  }
+};
+
+render(<Sukutaulu suku={sukuJson} />, document.getElementById('root'));
