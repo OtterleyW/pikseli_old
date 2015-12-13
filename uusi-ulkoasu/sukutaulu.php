d1b1fe157635d793fe295f4ca2e1afb1fe74e233
Vesa Laakso <vesa.laakso@venuu.fi>
2015-12-13 20:16:18 +0200

    Tuuttaa esimerkkihepon sukutaulu ulos sukutaulu.php:ssa

--- uusi-ulkoasu/sukutaulu.php
+++ uusi-ulkoasu/sukutaulu.php
@@ -3,6 +3,51 @@
 	require('luokat/Heppa.php');
 	require('yla.php');
 ?>
+
+<?php
+	function hae_tiedot($url, $db){
+		//Perustietojen hakeminen
+		$stmt = $db->prepare('SELECT * FROM hevonen_tiedot WHERE url = :url');
+		$stmt->bindParam(':url', $hevonen_url);
+		$hevonen_url = $url;
+		$stmt->execute();
+		$haettu_tiedot = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+		//Sukutietojen
+		$stmt = $db->prepare('SELECT * FROM hevonen_suku WHERE id = :id');
+		$stmt->bindParam(':id', $haettu_tiedot['id']);
+		$stmt->execute();
+		$haettu_suku = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		//Tämän Heppa-olion luonti
+		$tama_heppa = new Heppa($haettu_tiedot, $haettu_suku);
+		$tama_heppa->hae_sukupolvet($db, $tama_heppa->suvun_pituus);
+
+		return $tama_heppa;
+	}
+
+	function serialisoitava_suku($heppa) {
+		$return = array(
+			'name' => $heppa->nimi,
+			'url' => $heppa->url
+		);
+		if (isset($heppa->isa)) {
+			$return['father'] = serialisoitava_suku($heppa->isa);
+		}
+		if (isset($heppa->ema)) {
+			$return['mother'] = serialisoitava_suku($heppa->ema);
+		}
+		return $return;
+	}
+
+	$heppa = hae_tiedot('monte-rosa-thrill', $db);
+?>
+
+<script>
+window.VS_SUKU_JSON = <?= json_encode(serialisoitava_suku($heppa)) ?>;
+</script>
+
 <div id="vs-sukutaulu-root"></div>
 
 <?php
