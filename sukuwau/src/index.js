7d0bf45dddaca53d59386720cc3317c78e832106
Vesa Laakso <vesa.laakso@venuu.fi>
2015-12-13 20:15:52 +0200

    Hae eka sukutaulu VS_SUKU_JSON globaalista muuttujasta

--- sukuwau/src/index.js
+++ sukuwau/src/index.js
@@ -2,33 +2,35 @@ import React from 'react';
 import { render } from 'react-dom';
 import Sukutaulu from './Sukutaulu';
 
-const sukuJson = {
-  name: 'Heppa',
-  father: {
-    name: 'Isä',
-    father: {
-      name: 'Isän isä',
-      father: { name: 'Isän isän isä' },
-      mother: { name: 'Isän isän emä' } },
-    mother: {
-      name: 'Isän emä',
-      father: { name: 'Isän emän isä' },
-      mother: { name: 'Isän emän emä' }
-    }
-  },
-  mother: {
-    name: 'Emä',
-    father: {
-      name: 'Emän isä',
-      father: { name: 'Emän isän isä' },
-      mother: { name: 'Emän isän emä' }
-    },
-    mother: {
-      name: 'Emän emä',
-      father: { name: 'Emän emän isä' },
-      mother: { name: 'Emän emän emä' }
-    }
-  }
-};
+// const sukuJson = {
+//   name: 'Heppa',
+//   father: {
+//     name: 'Isä',
+//     father: {
+//       name: 'Isän isä',
+//       father: { name: 'Isän isän isä' },
+//       mother: { name: 'Isän isän emä' } },
+//     mother: {
+//       name: 'Isän emä',
+//       father: { name: 'Isän emän isä' },
+//       mother: { name: 'Isän emän emä' }
+//     }
+//   },
+//   mother: {
+//     name: 'Emä',
+//     father: {
+//       name: 'Emän isä',
+//       father: { name: 'Emän isän isä' },
+//       mother: { name: 'Emän isän emä' }
+//     },
+//     mother: {
+//       name: 'Emän emä',
+//       father: { name: 'Emän emän isä' },
+//       mother: { name: 'Emän emän emä' }
+//     }
+//   }
+// };
+
+const sukuJson = window.VS_SUKU_JSON;
 
 render(<Sukutaulu suku={sukuJson} />, document.getElementById('vs-sukutaulu-root'));
