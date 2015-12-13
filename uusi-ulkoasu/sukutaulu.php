0c1d97caca239b71e84d514af04c38f40e6e0614
Vesa Laakso <vesa.laakso@venuu.fi>
2015-12-13 19:32:32 +0200

    Lataa sukutaulun koodi webpack-dev-serverin kautta vain lokaalisti

--- uusi-ulkoasu/sukutaulu.php
+++ uusi-ulkoasu/sukutaulu.php
@@ -4,6 +4,25 @@
 	require('yla.php');
 ?>
 <div id="root"></div>
-<script src="http://localhost:3000/static/bundle.js"></script>
+
+<?php
+	// Koita ladata webpack-dev-serverin kautta localhostilla tiedostot,
+	// muutoin mene vain sukutaulu-kansion sisällöllä.
+	$whitelist = array('127.0.0.1', '::1');
+	if (
+		in_array($_SERVER['REMOTE_ADDR'], $whitelist) &&
+		($socket = @fsockopen('127.0.0.1', 3000, $errno, $errstr, 0.1))
+	) {
+		fclose($socket);
+?>
+		<script src="http://localhost:3000/static/bundle.js"></script>
+<?
+	} else {
+?>
+		<link rel="stylesheet" href="sukutaulu/main.css" />
+		<script src="sukutaulu/bundle.js"></script>
+<?
+	}
+?>
 
 <?php require('ala.php'); ?>
