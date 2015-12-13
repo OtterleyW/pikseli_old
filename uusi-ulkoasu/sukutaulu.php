382ac7ae167e3ecfaac416f65c231e9966cc54f3
Vesa Laakso <vesa.laakso@venuu.fi>
2015-12-13 14:02:43 +0200

    Lisää hukkapuron sivuille sukutaululle latauspiste

---
+++ uusi-ulkoasu/sukutaulu.php
@@ -0,0 +1,9 @@
+<?php
+	require('.yhdista.php');
+	require('luokat/Heppa.php');
+	require('yla.php');
+?>
+<div id="root"></div>
+<script src="http://localhost:3000/static/bundle.js"></script>
+
+<?php require('ala.php'); ?>
