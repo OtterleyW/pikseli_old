fe1252ffdb728f76eacaebcec8f28f2930219080
Vesa Laakso <vesa.laakso@venuu.fi>
2016-01-03 14:35:08 +0200

    Run matching tests when files change, JIT-compiling only needed files
    Superduperawesome DX!

---
+++ sukuwau/src/reducers/__test__/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undefined */
+
+import reducers from '../index';
+
+export default (test) => {
+  test('returns state for unknown actions', (t) => {
+    const state = { foo: 'bar' };
+    t.equal(reducers(state, {}), state);
+    t.end();
+  });
+
+  test('stores initial family tree', (t) => {
+    const payload = {
+      name: 'My name',
+      mother: {},
+      father: {}
+    };
+    const action = {
+      type: 'SET_INITIAL_FAMILY_TREE',
+      payload: payload
+    };
+    t.deepEqual(reducers(undefined, action), payload);
+    t.end();
+  });
+};
--- sukuwau/test/index.js
+++ sukuwau/test/index.js
@@ -2,12 +2,13 @@
 
 require('babel-register');
 var glob = require('glob');
+var test = require('tape-catch');
 
 glob('./src/**/__test__/*.js', function(er, files) {
   if (er) throw er;
 
   files.forEach(function(f) {
     // console.error('Running tests in ' + f);
-    require('../' + f);
+    require('../' + f).default(test);
   });
 });
