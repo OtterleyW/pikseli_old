54e75c30454c3d738968f3f398067f83c8cb3529
Vesa Laakso <vesa.laakso@venuu.fi>
2016-01-24 18:29:55 +0200

    Allow family tree length to be overridden

--- sukuwau/src/Sukutaulu.js
+++ sukuwau/src/Sukutaulu.js
@@ -4,16 +4,7 @@ import Sukulainen from './Sukulainen';
 
 import styles from './styles/Sukutaulu.css';
 
-const suvunPituus = (sukuObj, pituus = 0) => {
-  const { mother, father } = sukuObj;
-  if (mother && father) {
-    return Math.max(
-      suvunPituus(mother, pituus + 1),
-      suvunPituus(father, pituus + 1)
-    );
-  }
-  return pituus;
-};
+const SUVUN_PITUUS = window.VS_SUKU_PITUUS || 4;
 
 const mapStateToProps = (state) => ({
   suku: state.horses
@@ -22,33 +13,24 @@ const mapStateToProps = (state) => ({
 export class SukutauluUI extends Component {
   render() {
     const { suku } = this.props;
-    const maxPituus = suvunPituus(suku);
-    let pituus = 4;
-
-    if (pituus > maxPituus) {
-      console.error(
-        `Haluamasi pituus (${pituus}) ylitti palvelimelta tulleen maksimipituuden (${maxPituus}).`
-      );
-      pituus = maxPituus;
-    }
 
     return (
       <div className={styles.container}>
-        {this._renderHeppa(suku, pituus)}
+        {this._renderHeppa(suku)}
       </div>
     );
   }
 
-  _renderHeppa(heppa, pituus, polvi = 0, isMother = false) {
+  _renderHeppa(heppa, polvi = 0, isMother = false) {
     const { name: nimi, father, mother, id } = heppa;
 
     const key = `${id}-${polvi}-${isMother ? 'm' : 'f'}`;
 
-    if (polvi < pituus + 1) {
+    if (polvi < SUVUN_PITUUS + 1) {
       return (
-        <Sukulainen nimi={nimi} polvi={polvi} pituus={pituus} id={id} key={key}>
-          {this._renderHeppa(father || {}, pituus, polvi + 1, false)}
-          {this._renderHeppa(mother || {}, pituus, polvi + 1, true)}
+        <Sukulainen nimi={nimi} polvi={polvi} pituus={SUVUN_PITUUS} id={id} key={key}>
+          {this._renderHeppa(father || {}, polvi + 1, false)}
+          {this._renderHeppa(mother || {}, polvi + 1, true)}
         </Sukulainen>
       );
     }
