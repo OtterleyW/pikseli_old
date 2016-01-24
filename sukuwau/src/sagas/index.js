86ac02ec8a046d7ca1b5c582b7df34701d48c80a
Vesa Laakso <vesa.laakso@venuu.fi>
2016-01-24 16:43:50 +0200

    First working navigation

--- sukuwau/src/sagas/index.js
+++ sukuwau/src/sagas/index.js
@@ -1,10 +1,21 @@
 import { BROWSE_HORSE } from '../constants';
-import { take } from 'redux-saga';
+import { setInitialFamilyTree } from '../actions';
+import { take, put, call } from 'redux-saga';
+
+const headers = new Headers({
+  'Content-Type': 'application/json'
+});
+
+function* fetchHorse(id) {
+  const response = yield fetch(`/hukkapuro/suku/${id}`, {headers});
+  const json = yield response.json();
+  yield put(setInitialFamilyTree(json));
+}
 
 function* browseHorseDaemon() {
   while (true) { // eslint-disable-line no-constant-condition
     const nextAction = yield take(BROWSE_HORSE);
-    console.log(nextAction);
+    yield call(fetchHorse, nextAction.payload);
   }
 }
 
